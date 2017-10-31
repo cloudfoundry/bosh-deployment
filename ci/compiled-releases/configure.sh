c958685c9c3f1b82efdb27c4c98a1a33cd0cf618
dmitriy kalinin <dkalinin@pivotal.io>
2017-10-31 09:55:04 -0700

    bump to 3468.1 stemcell

--- ci/compiled-releases/configure.sh
+++ ci/compiled-releases/configure.sh
@@ -6,3 +6,17 @@ fly -t production set-pipeline \
  -p compiled-releases \
  -c ./pipeline.yml \
  -l <(lpass show --note "concourse:production pipeline:compiled-releases")
+
+fly -t production set-pipeline \
+ -p compiled-releases-3468 \
+ -c ./pipeline-3468.yml \
+ -l <(lpass show --note "concourse:production pipeline:compiled-releases")
+
+fly -t production check-resource -r compiled-releases-3468/bosh-release -f version:263.4.0
+fly -t production check-resource -r compiled-releases-3468/uaa-release -f version:52.2
+fly -t production check-resource -r compiled-releases-3468/credhub-release -f version:1.6.0
+fly -t production check-resource -r compiled-releases-3468/warden-cpi -f version:37
+fly -t production check-resource -r compiled-releases-3468/garden-linux -f version:0.342.0
+fly -t production check-resource -r compiled-releases-3468/garden-runc -f version:1.9.4
+fly -t production check-resource -r compiled-releases-3468/grootfs -f version:0.24.0
+fly -t production check-resource -r compiled-releases-3468/ubuntu-trusty-stemcell -f version:3468
