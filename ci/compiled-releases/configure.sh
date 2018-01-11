3b1c1647a44627dbaa904bf679bc82aef00f0694
dmitriy kalinin <dkalinin@pivotal.io>
2018-01-10 16:09:36 -0800

    bump compiled releases pileines

--- ci/compiled-releases/configure.sh
+++ ci/compiled-releases/configure.sh
@@ -9,11 +9,11 @@ fly -t production set-pipeline -n \
 
 fly -t production check-resource -r compiled-releases-3468/bosh-release -f version:263.4.0
 fly -t production check-resource -r compiled-releases-3468/uaa-release -f version:52.2
-fly -t production check-resource -r compiled-releases-3468/credhub-release -f version:1.6.0
-fly -t production check-resource -r compiled-releases-3468/warden-cpi -f version:37
-fly -t production check-resource -r compiled-releases-3468/garden-linux -f version:0.342.0
-fly -t production check-resource -r compiled-releases-3468/garden-runc -f version:1.9.4
-fly -t production check-resource -r compiled-releases-3468/grootfs -f version:0.24.0
+# fly -t production check-resource -r compiled-releases-3468/credhub-release -f version:1.6.0
+# fly -t production check-resource -r compiled-releases-3468/warden-cpi -f version:37
+# fly -t production check-resource -r compiled-releases-3468/garden-linux -f version:0.342.0
+# fly -t production check-resource -r compiled-releases-3468/garden-runc -f version:1.9.4
+# fly -t production check-resource -r compiled-releases-3468/grootfs -f version:0.24.0
 fly -t production check-resource -r compiled-releases-3468/ubuntu-trusty-stemcell -f version:3468
 
 fly -t production set-pipeline -n \
@@ -23,7 +23,7 @@ fly -t production set-pipeline -n \
 
 fly -t production check-resource -r compiled-releases-3445/bosh-release -f version:263
 fly -t production check-resource -r compiled-releases-3445/uaa-release -f version:45.4
-fly -t production check-resource -r compiled-releases-3445/credhub-release -f version:1.3.4
+# fly -t production check-resource -r compiled-releases-3445/credhub-release -f version:1.3.4
 fly -t production check-resource -r compiled-releases-3445/ubuntu-trusty-stemcell -f version:3445
 
 fly -t production set-pipeline -n \
@@ -33,7 +33,7 @@ fly -t production set-pipeline -n \
 
 fly -t production check-resource -r compiled-releases-3421/bosh-release -f version:262.4
 fly -t production check-resource -r compiled-releases-3421/uaa-release -f version:41.1
-fly -t production check-resource -r compiled-releases-3421/credhub-release -f version:1.0.8
+# fly -t production check-resource -r compiled-releases-3421/credhub-release -f version:1.0.8
 fly -t production check-resource -r compiled-releases-3421/ubuntu-trusty-stemcell -f version:3421
 
 fly -t production set-pipeline -n \
