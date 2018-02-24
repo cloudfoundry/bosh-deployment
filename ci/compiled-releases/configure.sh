daf26e7e7722452a7c5254fb25f23957d1670486
dmitriy kalinin <dkalinin@pivotal.io>
2018-02-23 21:10:48 -0800

    add 3541.x stemcell for compilatio

--- ci/compiled-releases/configure.sh
+++ ci/compiled-releases/configure.sh
@@ -3,6 +3,21 @@
 set -eu
 
 fly -t production set-pipeline -n \
+ -p compiled-releases-3541 \
+ -c ./pipeline-3541.yml \
+ -l <(lpass show --note "concourse:production pipeline:compiled-releases")
+
+fly -t production check-resource -r compiled-releases-3541/bosh-release -f version:263.4.0
+fly -t production check-resource -r compiled-releases-3541/uaa-release -f version:52.2
+fly -t production check-resource -r compiled-releases-3541/credhub-release -f version:1.6.0
+fly -t production check-resource -r compiled-releases-3541/warden-cpi -f version:37
+fly -t production check-resource -r compiled-releases-3541/garden-linux -f version:0.342.0
+fly -t production check-resource -r compiled-releases-3541/garden-runc -f version:1.9.4
+fly -t production check-resource -r compiled-releases-3541/grootfs -f version:0.24.0
+fly -t production check-resource -r compiled-releases-3541/backup-and-restore-sdk-release -f version:1.2.1
+fly -t production check-resource -r compiled-releases-3541/ubuntu-trusty-stemcell -f version:3541
+
+fly -t production set-pipeline -n \
  -p compiled-releases-3468 \
  -c ./pipeline-3468.yml \
  -l <(lpass show --note "concourse:production pipeline:compiled-releases")
@@ -10,10 +25,6 @@ fly -t production set-pipeline -n \
 fly -t production check-resource -r compiled-releases-3468/bosh-release -f version:263.4.0
 fly -t production check-resource -r compiled-releases-3468/uaa-release -f version:52.2
 # fly -t production check-resource -r compiled-releases-3468/credhub-release -f version:1.6.0
-# fly -t production check-resource -r compiled-releases-3468/warden-cpi -f version:37
-# fly -t production check-resource -r compiled-releases-3468/garden-linux -f version:0.342.0
-# fly -t production check-resource -r compiled-releases-3468/garden-runc -f version:1.9.4
-# fly -t production check-resource -r compiled-releases-3468/grootfs -f version:0.24.0
 fly -t production check-resource -r compiled-releases-3468/ubuntu-trusty-stemcell -f version:3468
 
 fly -t production set-pipeline -n \
