f5e5454f04f609c988a0f6d827686f9a14e3b4a7
Max Petersen <mpetersen@pivotal.io>
2018-10-25 15:55:53 -0700

    Remove LTS pipelines
    - 3363.x
    - 3421.x
    - 3445.x
    
    [#161071385](https://www.pivotaltracker.com/story/show/161071385)
    
    Co-authored-by: Belinda Liu <bliu@pivotal.io>

--- ci/compiled-releases/configure.sh
+++ ci/compiled-releases/configure.sh
@@ -46,32 +46,3 @@ fly -t production check-resource -r compiled-releases-3468/bosh-release -f versi
 fly -t production check-resource -r compiled-releases-3468/uaa-release -f version:52.2
 fly -t production check-resource -r compiled-releases-3468/credhub-release -f version:1.6.0
 fly -t production check-resource -r compiled-releases-3468/ubuntu-trusty-stemcell -f version:3468
-
-fly -t production set-pipeline -n \
- -p compiled-releases-3445 \
- -c ./pipeline-3445.yml \
- -l <(lpass show --note "concourse:production pipeline:compiled-releases")
-
-fly -t production check-resource -r compiled-releases-3445/bosh-release -f version:263
-fly -t production check-resource -r compiled-releases-3445/uaa-release -f version:45.4
-fly -t production check-resource -r compiled-releases-3445/credhub-release -f version:1.3.4
-fly -t production check-resource -r compiled-releases-3445/ubuntu-trusty-stemcell -f version:3445
-
-fly -t production set-pipeline -n \
- -p compiled-releases-3421 \
- -c ./pipeline-3421.yml \
- -l <(lpass show --note "concourse:production pipeline:compiled-releases")
-
-fly -t production check-resource -r compiled-releases-3421/bosh-release -f version:262.4
-fly -t production check-resource -r compiled-releases-3421/uaa-release -f version:41.1
-# fly -t production check-resource -r compiled-releases-3421/credhub-release -f version:1.0.8
-fly -t production check-resource -r compiled-releases-3421/ubuntu-trusty-stemcell -f version:3421
-
-fly -t production set-pipeline -n \
- -p compiled-releases-3363 \
- -c ./pipeline-3363.yml \
- -l <(lpass show --note "concourse:production pipeline:compiled-releases")
-
-fly -t production check-resource -r compiled-releases-3363/bosh-release -f version:260.8
-fly -t production check-resource -r compiled-releases-3363/uaa-release -f version:24.12
-fly -t production check-resource -r compiled-releases-3363/ubuntu-trusty-stemcell -f version:3363
