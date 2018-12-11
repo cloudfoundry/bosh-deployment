7d0d36132eedae88379ece7218bf79b9d162b0f1
Luan Santos <lsantos@pivotal.io>
2018-12-11 11:12:10 -0800

    Remove 3541 stemcell compiled releases

--- ci/compiled-releases/configure.sh
+++ ci/compiled-releases/configure.sh
@@ -25,14 +25,3 @@ fly -t production check-resource -r compiled-releases-3586/warden-cpi -f version
 fly -t production check-resource -r compiled-releases-3586/garden-linux -f version:0.342.0
 fly -t production check-resource -r compiled-releases-3586/garden-runc -f version:1.9.4
 fly -t production check-resource -r compiled-releases-3586/grootfs -f version:0.24.0
-
-fly -t production set-pipeline -n \
- -p compiled-releases-3541 \
- -c ./pipeline-3541.yml \
- -l <(lpass show --note "concourse:production pipeline:compiled-releases")
-
-fly -t production check-resource -r compiled-releases-3541/bosh-release -f version:263.4.0
-fly -t production check-resource -r compiled-releases-3541/uaa-release -f version:52.2
-fly -t production check-resource -r compiled-releases-3541/credhub-release -f version:1.6.0
-fly -t production check-resource -r compiled-releases-3541/backup-and-restore-sdk-release -f version:1.2.1
-fly -t production check-resource -r compiled-releases-3541/ubuntu-trusty-stemcell -f version:3541
