16bd5afb492b8f6e5e69a006f03aa0f4e715249a
Belinda Liu <bliu@pivotal.io>
2019-02-12 16:13:49 -0500

    Automate stemcell version bumps for compiled releases
    This commit triggers the compilation builds with every new stemcell
    version. It also adds a task to the existing compilation builds to write the
    version, url, and sha for its release into an intermediate vars file. A
    new build will then consolidate these versions into one file, which can
    then be used for interpolation. This way, the relevant manifests and ops
    files will always have the most up to date versions.
    
    [#163832577](https://www.pivotaltracker.com/story/show/163832577)
    
    Co-authored-by: Miguel Verissimo <mverissimo@pivotal.io>

---
+++ ci/tasks/consolidate-versions.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+git clone bosh-deployment bosh-deployment-output
+
+DIRECTORY=bosh-deployment/ci/intermediate-vars/*.yml
+
+echo "---" > version-vars.yml
+for file in "${DIRECTORY}"/*.yml; do
+  tail -n 4 "${file}" >> version-vars.yml
+done
+
+pushd $PWD/bosh-deployment-output
+  git diff
+  git add -A
+  git config --global user.email "ci@localhost"
+  git config --global user.name "CI Bot"
+  git commit -m "Bumping BOSH to version $VERSION"
+popd
---
+++ ci/tasks/extract-version.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+tar -xzf compiled-release/*.tgz $( tar -tzf *.tgz | grep 'release.MF' )
+RELEASE_NAME=$( grep -E '^name: ' release.MF | awk '{print $2}' | tr -d "\"'" )
+# ''"
+RELEASE_VERSION=$( grep -E '^version: ' release.MF | awk '{print $2}' | tr -d "\"'" )
+# ''"
+SHA=($(sha1sum *.tgz))
+URL="https://s3.amazonaws.com/bosh-compiled-release-tarballs/${SHA[1]}"
+
+VERSION_FILE=ci/intermediate-vars/${RELEASE_NAME}-vars.yml
+
+git clone bosh-deployment bosh-deployment-output
+
+cat << EOF > $VERSION_FILE
+---
+- name: $RELEASE_NAME
+  version: $RELEASE_VERSION
+  url: $URL
+  sha: ${SHA[0]}
+EOF
+
+pushd $PWD/bosh-deployment-output
+  git diff
+  git add -A
+  git config --global user.email "ci@localhost"
+  git config --global user.name "CI Bot"
+  git commit -m "Bumping ${RELEASE_NAME} to version ${VERSION}"
+popd
