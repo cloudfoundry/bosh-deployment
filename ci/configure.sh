eb4c143b01745cef102b38e56d330d4fd2de396a
Joseph Palermo <jpalermo@pivotal.io>
2021-06-25 15:30:42 -0700

    Move to new concourse

--- ci/configure.sh
+++ ci/configure.sh
@@ -4,7 +4,7 @@ set -eu
 
 lpass ls > /dev/null
 
-fly -t director set-pipeline -p bosh-deployment \
+fly -t bosh-ecosystem set-pipeline -p bosh-deployment \
     -c ci/pipeline.yml \
     -l <(lpass show -G "bosh-deployment concourse secrets" --notes) \
     -l <(lpass show --note "concourse:production pipeline:compiled-releases")
