7c137ad8ecf23f88d0786a83c253d9453ef80972
ramonskie <ramonmakkelie@gmail.com>
2025-10-09 19:10:17 +0200

    if the opsfile does not exists don't use bosh int

--- ci/tasks/update-stemcell.sh
+++ ci/tasks/update-stemcell.sh
@@ -12,7 +12,11 @@ git clone bosh-deployment bosh-deployment-output
 
 UPDATE_STEMCELL_OPSFILE=$(make_stemcell_opsfile $URL $SHA)
 
-bosh int bosh-deployment/${CPI_OPS_FILE} -o $UPDATE_STEMCELL_OPSFILE > bosh-deployment-output/${CPI_OPS_FILE}
+if [ -f "bosh-deployment/${CPI_OPS_FILE}" ]; then
+  bosh int bosh-deployment/${CPI_OPS_FILE} -o $UPDATE_STEMCELL_OPSFILE > bosh-deployment-output/${CPI_OPS_FILE}
+else
+  cp $UPDATE_STEMCELL_OPSFILE bosh-deployment-output/${CPI_OPS_FILE}
+fi
 
 pushd $PWD/bosh-deployment-output
   git_commit "Bumping Stemcell $STEMCELL_NAME to version $VERSION"
